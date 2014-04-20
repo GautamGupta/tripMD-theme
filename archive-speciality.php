7883f17f63961c86fa4601d69ad32e257053375a
Gautam Gupta <gautam@gaut.am>
2014-04-20 11:50:27 +0530

    Use featured image

--- archive-speciality.php
+++ archive-speciality.php
@@ -22,7 +22,10 @@ get_header(); ?>
 
 				<?php while ( have_posts() ) : the_post(); ?>
 
-					<a href="<?php the_permalink(); ?>" id="<?php echo get_post_type(); ?>-<?php the_ID(); ?>" <?php post_class( 'card' ); ?> rel="bookmark"><img src="<?php echo get_template_directory_uri(); ?>/img/cardiac.png" alt=""><h3><?php the_title(); ?></h3></a>
+					<a href="<?php the_permalink(); ?>" id="<?php echo get_post_type(); ?>-<?php the_ID(); ?>" <?php post_class( 'card' ); ?> rel="bookmark">
+						<?php if ( has_post_thumbnail() ) the_post_thumbnail(); ?>
+						<h3><?php the_title(); ?></h3>
+					</a>
 
 				<?php endwhile; ?>
 
