9eb045f8ed82a87b16b844541cea8bb73136ad9d
Aneesh Dogra <lionaneesh@gmail.com>
2014-05-10 23:30:54 +0530

    Make some changes to the doctor's page.

--- single-doctor.php
+++ single-doctor.php
@@ -16,16 +16,28 @@ get_header(); ?>
 
 <?php while ( have_posts() ) : the_post(); ?>
 
-	<h2 class="animated fadeIn">Dr. <?php the_title(); ?></h2>
+	<h2 class="animated fadeIn"><?php the_title(); ?></h2>
 
 	<div class="content">
 
 		<?php if ( has_post_thumbnail() ) the_post_thumbnail( 'full', array( 'class' => 'alignright' ) ); ?>
-
+		<?php endif; ?>
+		<?php else if ( get_post_meta( get_the_ID(), 'picture_url', true ) ) : ?>
+			<img src="<php echo get_post_meta( get_the_ID(), 'picture_url', true ); ?>" class="alignright wp-post-image" width="200px"/>
+		<?php endif; ?>
 		<?php the_content(); ?>
 
+
+		<?php if ( get_post_meta( get_the_ID(), 'location', true ) ) : ?>
+			<p><strong>Location</strong>: <?php echo get_post_meta( get_the_ID(), 'location', true ); ?></p>
+		<?php endif; ?>
+
+		<?php if ( get_post_meta( get_the_ID(), 'specialization', true ) ) : ?>
+			<p><strong>Specialization</strong>: <?php echo get_post_meta( get_the_ID(), 'specialization', true ); ?></p>
+		<?php endif; ?>
+
 		<?php if ( get_post_meta( get_the_ID(), 'experience', true ) ) : ?>
-			<p><strong>Experience</strong>: <?php echo get_post_meta( get_the_ID(), 'experience', true ); ?>+ years</p>
+			<p><strong>Experience</strong>: <?php echo get_post_meta( get_the_ID(), 'experience', true ); ?></p>
 		<?php endif; ?>
 		<?php if ( get_post_meta( get_the_ID(), 'qualifications', true ) ) : ?>
 			<p><strong>Qualifications</strong>: <?php echo get_post_meta( get_the_ID(), 'qualifications', true ); ?></p>
