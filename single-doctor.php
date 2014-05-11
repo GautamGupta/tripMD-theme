8649ac8db9d47880141bca0bd425e8061d3aada2
Aneesh Dogra <lionaneesh@gmail.com>
2014-05-11 15:37:01 +0530

    Doctors: Fixes.

--- single-doctor.php
+++ single-doctor.php
@@ -21,7 +21,6 @@ get_header(); ?>
 	<div class="content">
 
 		<?php if ( has_post_thumbnail() ) the_post_thumbnail( 'full', array( 'class' => 'alignright' ) ); ?>
-		<?php endif; ?>
 		<?php else if ( get_post_meta( get_the_ID(), 'picture_url', true ) ) : ?>
 			<img src="<php echo get_post_meta( get_the_ID(), 'picture_url', true ); ?>" class="alignright wp-post-image" width="200px"/>
 		<?php endif; ?>
