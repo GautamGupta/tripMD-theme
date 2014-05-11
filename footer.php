daac49b4c3d2ca6bcf0f66f174400dc7265ddfa5
Gautam Gupta <gautam@gaut.am>
2014-05-11 20:37:08 +0530

    Home footer

--- footer.php
+++ footer.php
@@ -17,7 +17,7 @@
 			</main>
 
 		</section>
-
+-->
 		<footer id="colophon" class="site-footer" role="contentinfo">
 
 			<div class="grid-container block grid-100">
@@ -26,7 +26,7 @@
 
 					<div class="grid-100">
 
-						<p>&copy; <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><img src="<?php echo get_template_directory_uri(); ?>/img/logo-<?php echo is_home() ? 'white' : 'black'; ?>.png" alt="<?php bloginfo( 'name' ); ?>"></a> <?php echo date( 'Y' ); ?></p>
+						<p>&copy; <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><img src="<?php echo get_template_directory_uri(); ?>/img/logo-black.png" alt="<?php bloginfo( 'name' ); ?>"></a> <?php echo date( 'Y' ); ?></p>
 
 					</div>
 
@@ -35,7 +35,7 @@
 			</div>
 
 		</footer>
--->
+
 		<?php wp_footer(); ?>
 
 		<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
