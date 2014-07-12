2663311ea76e50e664b2876dad680690bc932fc4
Gautam Gupta <gautam@gaut.am>
2014-07-12 12:19:57 +0530

    Fix smooth scrolling

--- js/js.js
+++ js/js.js
@@ -28,8 +28,8 @@ window.onscroll = function() {
     }
 }
 
-// Royal Slider
 jQuery(document).ready(function($) {
+  // Royal Slider
   jQuery.rsCSS3Easing.easeOutBack = 'cubic-bezier(0.175, 0.885, 0.320, 1.275)';
   $('#slider-with-blocks-1').royalSlider({
 	arrowsNav: true,
@@ -66,7 +66,7 @@ jQuery(document).ready(function($) {
     controlNavigation: 'tabs',
     imageScaleMode: 'fill',
     imageAlignCenter:false,
-    loop: false,
+    loop: true,
     loopRewind: true,
     numImagesToPreload: 6,
     keyboardNavEnabled: false,
@@ -75,25 +75,22 @@ jQuery(document).ready(function($) {
 
   $('#content-slider-1').prepend($('#content-slider-1').find('.rsNav'));
 
-});
-
-// Timeline page
-var $timeline_block = $('.cd-timeline-block');
-
-// Hide timeline blocks which are outside the viewport
-$timeline_block.each(function(){
-    if($(this).offset().top > $(window).scrollTop()+$(window).height()*0.75) {
-        $(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
-    }
-});
-
-// On scolling, show/animate timeline blocks when enter the viewport
-$(window).on('scroll', function(){
-    $timeline_block.each(function(){
-        if( $(this).offset().top <= $(window).scrollTop()+$(window).height()*0.75 && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) {
-            $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
+    // Hide timeline blocks which are outside the viewport
+    $('.cd-timeline-block').each(function(){
+        if($(this).offset().top > $(window).scrollTop()+$(window).height()*0.75) {
+            $(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
         }
     });
+
+    // On scolling, show/animate timeline blocks when enter the viewport
+    $(window).on('scroll', function(){
+        $timeline_block.each(function(){
+            if( $(this).offset().top <= $(window).scrollTop()+$(window).height()*0.75 && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) {
+                $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
+            }
+        });
+    });
+
 });
 
 // Smooth scrolling by some guy
