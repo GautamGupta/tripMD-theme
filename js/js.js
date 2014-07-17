8b733ce392b858df4b6b92a716448c5b93be4cda
Gautam Gupta <gautam@gaut.am>
2014-07-17 17:27:16 +0530

    Fix selectbox hide/show in affordable section on front page.

--- js/js.js
+++ js/js.js
@@ -30,20 +30,18 @@ window.onscroll = function() {
 
 jQuery(document).ready(function($) {
 
+    // Speciality selection
+    // http://tripmd.com/wp-admin/admin-ajax.php?action=tmd_api&method=procedures_costs&ver=0.1
     $(".spec-select").change(function(){
         // alert(this.value);
-        $(".sub#" + this.value + " + a").css({"visibility":"visible", "opacity":"1.0"});
-        // document.querySelectorAll(".sub#" + this.value + " + a")[0].style.display = "block !important";
-   
+        $(".sub + a").css({"visibility":"hidden", "opacity":"0.0", "display": "none"});
+        $(".sub#" + this.value + " + a").css({"visibility":"visible", "opacity":"1.0", "display": "block"});
+
     });
-
-
     $("select.sub").change(function(){
         // alert(this.value);
         $(".int").css({"opacity":"1.0"});
         $(".pred").css({"opacity":"1.0"});
-        // document.querySelectorAll(".sub#" + this.value + " + a")[0].style.display = "block !important";
-   
     });
 
   // Royal Slider
@@ -125,7 +123,6 @@ jQuery(document).ready(function($) {
 
 });
 
-
 // Expandable textbox
 function expandtext( textArea, min, max ) {
     min = typeof min !== 'undefined' ? min : 1;
@@ -145,9 +142,6 @@ function expandtext( textArea, min, max ) {
     if (textArea.rows <= max)
         textArea.rows++
 }
-
-// http://tripmd.com/wp-admin/admin-ajax.php?action=tmd_api&method=procedures_costs&ver=0.1
-
 
 // Smooth scrolling by some guy
 Scroller={speed:10,gy:function(e){var t=e.offsetTop;if(e.offsetParent)while(e==e.offsetParent)t+=e.offsetTop;return t},scrollTop:function(){var e=document.body,t=document.documentElement;return e&&e.scrollTop?e.scrollTop:t&&t.scrollTop?t.scrollTop:window.pageYOffset?window.pageYOffset:0},add:function(e,t,n){if(e.addEventListener)return e.addEventListener(t,n,!1);if(e.attachEvent)return e.attachEvent("on"+t,n)},end:function(e){if(window.event){window.event.cancelBubble=!0,window.event.returnValue=!1;return}e.preventDefault&&e.stopPropagation&&(e.preventDefault(),e.stopPropagation())},scroll:function(e){var t=window.innerHeight||document.documentElement.clientHeight,n=document.body.scrollHeight,r=Scroller.scrollTop();n-e<t&&(e=n-t),e>r?r+=Math.ceil((e-r)/Scroller.speed):r+=(e-r)/Scroller.speed,window.scrollTo(0,r),(r==e||Scroller.previousPos==r)&&clearInterval(Scroller.interval),Scroller.previousPos=r},init:function(){Scroller.add(window,"load",Scroller.render)},render:function(){Scroller.end(this);var e=document.getElementsByTagName("a");for(var t=0;t<e.length;t++){var n=e[t];n.href&&n.href.indexOf("#")!=-1&&n.href.indexOf("#")!=n.href.length-1&&(n.pathname==location.pathname||"/"+n.pathname==location.pathname)&&Scroller.add(n,"click",function(){Scroller.end(this);var t=this.hash.substr(1),n=document.getElementById(t);if(!n)for(var r=0;r<e.length;r++)if(e[r].name==t){n=e[r];break}n&&(clearInterval(Scroller.interval),Scroller.interval=setInterval(function(){Scroller.scroll(Scroller.gy(n))},10))})}}},Scroller.init();
