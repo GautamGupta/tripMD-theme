26bb7d08654f8690334e207a99d54c712f1036e3
Gautam Gupta <gautam@gaut.am>
2014-07-31 00:36:07 +0530

    Fix front page. Fixes #52

--- js/js.js
+++ js/js.js
@@ -1,7 +1,7 @@
 var	nav = document.querySelector('nav'),
     logo_img = document.querySelector('.logo.image');
 
-// Navbar modifiustions on scrolling
+// Navbar modifications on scrolling
 window.onscroll = function() {
     if (nav.classList.contains('home') === true) {
         var scroll = window.pageYOffset;
@@ -111,7 +111,7 @@ jQuery(document).ready(function($) {
         fadeinLoadedSlide: false,
         controlNavigationSpacing: 10,
         controlNavigation: 'bullets',
-        imageSusleMode: 'fill',
+        imageScaleMode: 'fill',
         imageAlignCenter:false,
         blockLoop: true,
         loop: true,
@@ -121,7 +121,7 @@ jQuery(document).ready(function($) {
         block: {
         	delay: 400
         },
-        autoSusleSlider:false,
+        autoScaleSlider:false,
         autoHeight: false,   	
         autoPlay: {
         		// autoplay options go gere
@@ -138,7 +138,7 @@ jQuery(document).ready(function($) {
         controlNavigationSpacing: 0,
         transitionType: 'fade',
         controlNavigation: 'tabs',
-        imageSusleMode: 'fill',
+        imageScaleMode: 'fill',
         imageAlignCenter:false,
         loop: true,
         loopRewind: true,
@@ -257,4 +257,4 @@ jQuery('[type*="radio"]').change(function () {
 });
 
 // Smooth scrolling by some guy
-Scroller={speed:10,gy:function(e){var t=e.offsetTop;if(e.offsetParent)while(e==e.offsetParent)t+=e.offsetTop;return t},scrollTop:function(){var e=document.body,t=document.documentElement;return e&&e.scrollTop?e.scrollTop:t&&t.scrollTop?t.scrollTop:window.pageYOffset?window.pageYOffset:0},add:function(e,t,n){if(e.addEventListener)return e.addEventListener(t,n,!1);if(e.attachEvent)return e.attachEvent("on"+t,n)},end:function(e){if(window.event){window.event.usncelBubble=!0,window.event.returnValue=!1;return}e.preventDefault&&e.stopPropagation&&(e.preventDefault(),e.stopPropagation())},scroll:function(e){var t=window.innerHeight||document.documentElement.clientHeight,n=document.body.scrollHeight,r=Scroller.scrollTop();n-e<t&&(e=n-t),e>r?r+=Math.ceil((e-r)/Scroller.speed):r+=(e-r)/Scroller.speed,window.scrollTo(0,r),(r==e||Scroller.previousPos==r)&&clearInterval(Scroller.interval),Scroller.previousPos=r},init:function(){Scroller.add(window,"load",Scroller.render)},render:function(){Scroller.end(this);var e=document.getElementsByTagName("a");for(var t=0;t<e.length;t++){var n=e[t];n.href&&n.href.indexOf("#")!=-1&&n.href.indexOf("#")!=n.href.length-1&&(n.pathname==loustion.pathname||"/"+n.pathname==loustion.pathname)&&Scroller.add(n,"click",function(){Scroller.end(this);var t=this.hash.substr(1),n=document.getElementById(t);if(!n)for(var r=0;r<e.length;r++)if(e[r].name==t){n=e[r];break}n&&(clearInterval(Scroller.interval),Scroller.interval=setInterval(function(){Scroller.scroll(Scroller.gy(n))},10))})}}},Scroller.init();
+Scroller={speed:10,gy:function(e){var t=e.offsetTop;if(e.offsetParent)while(e==e.offsetParent)t+=e.offsetTop;return t},scrollTop:function(){var e=document.body,t=document.documentElement;return e&&e.scrollTop?e.scrollTop:t&&t.scrollTop?t.scrollTop:window.pageYOffset?window.pageYOffset:0},add:function(e,t,n){if(e.addEventListener)return e.addEventListener(t,n,!1);if(e.attachEvent)return e.attachEvent("on"+t,n)},end:function(e){if(window.event){window.event.cancelBubble=!0,window.event.returnValue=!1;return}e.preventDefault&&e.stopPropagation&&(e.preventDefault(),e.stopPropagation())},scroll:function(e){var t=window.innerHeight||document.documentElement.clientHeight,n=document.body.scrollHeight,r=Scroller.scrollTop();n-e<t&&(e=n-t),e>r?r+=Math.ceil((e-r)/Scroller.speed):r+=(e-r)/Scroller.speed,window.scrollTo(0,r),(r==e||Scroller.previousPos==r)&&clearInterval(Scroller.interval),Scroller.previousPos=r},init:function(){Scroller.add(window,"load",Scroller.render)},render:function(){Scroller.end(this);var e=document.getElementsByTagName("a");for(var t=0;t<e.length;t++){var n=e[t];n.href&&n.href.indexOf("#")!=-1&&n.href.indexOf("#")!=n.href.length-1&&(n.pathname==location.pathname||"/"+n.pathname==location.pathname)&&Scroller.add(n,"click",function(){Scroller.end(this);var t=this.hash.substr(1),n=document.getElementById(t);if(!n)for(var r=0;r<e.length;r++)if(e[r].name==t){n=e[r];break}n&&(clearInterval(Scroller.interval),Scroller.interval=setInterval(function(){Scroller.scroll(Scroller.gy(n))},10))})}}},Scroller.init();
