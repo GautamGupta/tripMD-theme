834d7584e681a9d2a7b635ee6e33412765f6c008
Gautam Gupta <gautam@gaut.am>
2014-10-08 23:01:36 -0400

    Exit after redirect

--- includes/speciality.php
+++ includes/speciality.php
@@ -2,7 +2,9 @@
 
 function tmd_speciality_redirect() {
 
-    if ( tmd_is_speciality() && !is_user_logged_in() )
+    if ( tmd_is_speciality() && !is_user_logged_in() ) {
         wp_redirect( add_query_arg( array( 'redirect_to' => get_permalink() ), site_url( '/register' ) ) );
+        exit;
+    }
 
 }
