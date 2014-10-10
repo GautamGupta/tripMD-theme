4199f15e599d991a63900bb5f2e70ce77f5888d0
Gautam Gupta <gautam@gaut.am>
2014-10-10 14:53:23 -0400

    Comment out reviews pagination for now.

--- comments-doctor-reviews.php
+++ comments-doctor-reviews.php
@@ -27,6 +27,14 @@ if ( post_password_required() || !have_comments() )
             ) );
         ?>
 
+        <?php /*if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
+        <nav id="comment-nav-below" class="comment-navigation" role="navigation">
+            <h1 class="screen-reader-text"><?php _e( 'Review navigation', 'tripmd' ); ?></h1>
+            <div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Reviews', 'tripmd' ) ); ?></div>
+            <div class="nav-next"><?php next_comments_link( __( 'Newer Reviews &rarr;', 'tripmd' ) ); ?></div>
+        </nav><!-- #comment-nav-below -->
+        <?php endif; // check for comment navigation */ ?>
+
     </div>
 
 </header>
